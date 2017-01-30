6973a0f15e186938bfccc443eb2e7255e2b6e99b
eldoo <eldoo@users.noreply.github.com>
2017-01-30 08:50:28 +0100

    Delete obsolete execute plugin code

--- src/query_engine/oph_query_plugin_executor.h
+++ src/query_engine/oph_query_plugin_executor.h
@@ -36,45 +36,6 @@
 void (*_oph_plugin_reset)(UDF_INIT*, UDF_ARGS*, char*, char*);
 
 /**
- * \brief			          Structure to retrieve data where plugin is applied 
- * \param groups 	      Group of NULL-terminated record sets (record arrays)
- * \param group_number	Number of records per group
- */
-typedef struct {
-  oph_iostore_frag_record ***groups;  
-  int group_number;
-} oph_selection;
-
-/**
- * \brief		            Structure to store description of a plugin argument
- * \param arg_type 	    Type of argument
- * \param arg_length    Argument length
- * \param arg_value     Argument value
- * \param arg_pointer   Pointer to selected data related to argument 
- */
-typedef struct {
-  enum Item_result arg_type;
-  unsigned long arg_length; 
-  char *arg_value;
-  oph_selection *arg_pointer;
-} oph_udf_arg;
-
-/**
- * \brief               Function used to assign a selects recordset to oph_selection structure
- * \param select        Pointer where oph_selection structure will be created
- * \param recordset     Pointer to recordset array to be assigned
- * \return              0 if successfull, non-0 otherwise
- */
-int oph_select_data(oph_selection **select, oph_iostore_frag_record **recordset);
-
-/**
- * \brief               Function used to free oph_select structure
- * \param select        Pointer to oph_selection structure to be freed
- * \return              0 if successfull, non-0 otherwise
- */
-int oph_free_select_data(oph_selection *select);
-
-/**
  * \brief               Function used to free UDF_ARG argument
  * \param arguments     Pointer to UDF_ARG structure to be freed
  * \return              0 if successfull, non-0 otherwise
@@ -94,64 +55,6 @@ int free_udf_arg(UDF_ARGS *arguments);
  * \return            0 if successfull, non-0 otherwise
  */
 int _oph_execute_plugin(const oph_plugin *plugin, UDF_ARGS *args, UDF_INIT *initid, void **res, unsigned long long *res_length, char *is_null, char *error, char *result,  oph_plugin_api *functions);
-
-/**
- * \brief                 Function used to run all the stesp of a UDF plugin (extended version)
- * \param plugin          Plugin to be executed
- * \param args            Arguments used as plugin input
- * \param arg_count       Number of arguments used as plugin input
- * \param result_set      Pointer used for output record set
- * \param field_list_num  Number of fields in input recordset
- * \param id_col_index    Index of column containing id values
- * \param col_index       Index of column containing measure values
- * \param limit           Number of rows to be considered by the plugin
- * \param offset          First row to be considered by the plugin
- * \param omp_thread_num  Number of OpenMP threads to be used
- * \return                0 if successfull, non-0 otherwise
- */
-int oph_execute_plugin(const oph_plugin *plugin, oph_udf_arg *args, unsigned int arg_count, oph_iostore_frag_record_set **result_set, int field_list_num, int id_col_index, int col_index, long long limit, long long offset, unsigned short omp_thread_num);
-
-/**
- * \brief                 Function used to run all the stesp of a UDF plugin
- * \param plugin          Plugin to be executed
- * \param args            Arguments used as plugin input
- * \param arg_count       Number of arguments used as plugin input
- * \param result_set      Pointer used for output record set
- * \param field_list_num  Number of fields in input recordset
- * \param col_index       Index of column containing measure values
- * \param row_number      Number of rows to be considered by the plugin
- * \param offset          First row to be considered by the plugin
- * \param omp_thread_num  Number of OpenMP threads to be used
- * \return                0 if successfull, non-0 otherwise
- */
-int oph_execute_plugin2(const oph_plugin *plugin, oph_udf_arg *args, unsigned int arg_count, oph_iostore_frag_record_set *result_set, int field_list_num, int col_index, long long row_number, long long offset, unsigned short omp_thread_num);
-
-/**
- * \brief       Function to set up oph_udf_arg from the query parameter
- * \param param Query parameter
- * \param args  Pointer to arg structure to set up
- * \return      0 if successfull, non-0 otherwise
- */
-int oph_set_udf_arg(const char *param, oph_udf_arg *arg);
-
-/**
- * \brief       Function to free oph_udf_arg
- * \param args  Pointer to arg structure to free
- * \return      0 if successfull, non-0 otherwise
- */
-int oph_free_udf_arg(oph_udf_arg *arg);
-
-/**
- * \brief               Function to get plugin library and arguments from query string 
- * \param query_string  Query string with plugin expression to be extracted
- * \param plugin_table  Hash table with plugin libraries available
- * \param record_set    Recordset where plugin should be applied
- * \param plugin        Pointer to plugin library selected
- * \param args          Array of plugin arguments
- * \param args_count    Number of plugin arguments 
- * \return              0 if successfull, non-0 otherwise
- */
-int oph_parse_plugin(const char* query_string, HASHTBL *plugin_table, oph_iostore_frag_record_set *record_set, oph_plugin **plugin, oph_query_arg **stmt_args, oph_udf_arg **args, unsigned int *arg_count);
 
 /**
  * \brief               Function to run plugin CLEAR function 
